168faa0703ad5a4072bb6883530f562f63e27c27
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-06 11:26:05 +0800

    replace Grid with Box

--- src/components/navBar/NavLinks.jsx
+++ src/components/navBar/NavLinks.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import More from './More';
 import Advertise from '../dialogs/Advertise';
 import AddProducts from '../dialogs/AddProducts';
-import { Button, IconButton, Tooltip, Zoom, Grid, Box } from '@material-ui/core';
+import { Button, IconButton, Tooltip, Zoom, Box } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 export default function NavLinks() {
@@ -22,42 +22,28 @@ export default function NavLinks() {
 
 	return (
 		<Box flex="1 0 0%">
-			<Box marginRight={2}>
-				<Grid container spacing={1} justify="flex-end" alignItems="center">
-					<Grid item>
-						<Tooltip title="Support us on Patreon" TransitionComponent={Zoom} arrow>
-							<Button>Donate</Button>
-						</Tooltip>
-					</Grid>
-					<Grid item>
-						<Tooltip
-							title="Promote your brand on the Guide"
-							TransitionComponent={Zoom}
-							arrow
-						>
-							<Button onClick={openAdvertise}>Advertise</Button>
-						</Tooltip>
-					</Grid>
-					<Grid item>
-						<Tooltip title="Add products to the Guide" TransitionComponent={Zoom} arrow>
-							<IconButton
-								aria-label="Contribute to the Guide"
-								aria-controls="add-products"
-								aria-haspopup="true"
-								onClick={openAddProducts}
-								edge="end"
-							>
-								<AddIcon />
-							</IconButton>
-						</Tooltip>
-					</Grid>
-					<Grid item>
-						<More />
-					</Grid>
-				</Grid>
-				<Advertise open={openDialog === 'Advertise'} onClose={closeDialog} />
-				<AddProducts open={openDialog === 'AddProducts'} onClose={closeDialog} />
+			<Box marginRight={2} display="flex" justifyContent="flex-end" alignItems="center">
+				<Tooltip title="Support us on Patreon" TransitionComponent={Zoom} arrow>
+					<Button>Donate</Button>
+				</Tooltip>
+				<Tooltip title="Promote your brand on the Guide" TransitionComponent={Zoom} arrow>
+					<Button onClick={openAdvertise}>Advertise</Button>
+				</Tooltip>
+				<Tooltip title="Add products to the Guide" TransitionComponent={Zoom} arrow>
+					<IconButton
+						aria-label="Contribute to the Guide"
+						aria-controls="add-products"
+						aria-haspopup="true"
+						onClick={openAddProducts}
+						edge="end"
+					>
+						<AddIcon />
+					</IconButton>
+				</Tooltip>
+				<More />
 			</Box>
+			<Advertise open={openDialog === 'Advertise'} onClose={closeDialog} />
+			<AddProducts open={openDialog === 'AddProducts'} onClose={closeDialog} />
 		</Box>
 	);
 }
