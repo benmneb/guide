90844d521d659cdd38b11d2eb3db4d3aa1ba2a06
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-06 11:20:59 +0800

    replace ul with Grid

--- src/components/navBar/NavLinks.jsx
+++ src/components/navBar/NavLinks.jsx
@@ -2,32 +2,10 @@ import React, { useState } from 'react';
 import More from './More';
 import Advertise from '../dialogs/Advertise';
 import AddProducts from '../dialogs/AddProducts';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button, IconButton, Tooltip, Zoom, Box } from '@material-ui/core';
+import { Button, IconButton, Tooltip, Zoom, Grid, Box } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
-const useStyles = makeStyles((theme) => ({
-	container: {
-		'& ul': {
-			listStyleType: 'none',
-			padding: theme.spacing(2),
-			display: 'flex',
-			justifyContent: 'flex-end',
-			alignItems: 'center'
-		},
-		'& li': {
-			display: 'inline',
-			margin: theme.spacing(0, 0.5)
-		},
-		'& li:nth-last-child(2)': {
-			marginRight: 0
-		}
-	}
-}));
-
-const NavLinks = () => {
-	const styles = useStyles();
-
+export default function NavLinks() {
 	const [openDialog, setOpenDialog] = useState(null);
 
 	const openAdvertise = () => {
@@ -43,43 +21,43 @@ const NavLinks = () => {
 	};
 
 	return (
-		<Box flex="1 0 0%" className={styles.container}>
-			<ul>
-				<li>
-					<Tooltip title="Support us on Patreon" TransitionComponent={Zoom} arrow>
-						<Button>Donate</Button>
-					</Tooltip>
-				</li>
-				<li>
-					<Tooltip
-						title="Promote your brand on the Guide"
-						TransitionComponent={Zoom}
-						arrow
-					>
-						<Button onClick={openAdvertise}>Advertise</Button>
-					</Tooltip>
-				</li>
-				<li>
-					<Tooltip title="Add products to the Guide" TransitionComponent={Zoom} arrow>
-						<IconButton
-							aria-label="Contribute to the Guide"
-							aria-controls="add-products"
-							aria-haspopup="true"
-							onClick={openAddProducts}
-							edge="end"
+		<Box flex="1 0 0%">
+			<Box marginRight={2}>
+				<Grid container spacing={1} justify="flex-end" alignItems="center">
+					<Grid item>
+						<Tooltip title="Support us on Patreon" TransitionComponent={Zoom} arrow>
+							<Button>Donate</Button>
+						</Tooltip>
+					</Grid>
+					<Grid item>
+						<Tooltip
+							title="Promote your brand on the Guide"
+							TransitionComponent={Zoom}
+							arrow
 						>
-							<AddIcon />
-						</IconButton>
-					</Tooltip>
-				</li>
-				<li>
-					<More />
-				</li>
-			</ul>
-			<Advertise open={openDialog === 'Advertise'} onClose={closeDialog} />
-			<AddProducts open={openDialog === 'AddProducts'} onClose={closeDialog} />
+							<Button onClick={openAdvertise}>Advertise</Button>
+						</Tooltip>
+					</Grid>
+					<Grid item>
+						<Tooltip title="Add products to the Guide" TransitionComponent={Zoom} arrow>
+							<IconButton
+								aria-label="Contribute to the Guide"
+								aria-controls="add-products"
+								aria-haspopup="true"
+								onClick={openAddProducts}
+								edge="end"
+							>
+								<AddIcon />
+							</IconButton>
+						</Tooltip>
+					</Grid>
+					<Grid item>
+						<More />
+					</Grid>
+				</Grid>
+				<Advertise open={openDialog === 'Advertise'} onClose={closeDialog} />
+				<AddProducts open={openDialog === 'AddProducts'} onClose={closeDialog} />
+			</Box>
 		</Box>
 	);
-};
-
-export default NavLinks;
+}
