e23cf210d7a887ef82588b1700c1dfed4385a3d2
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-10 10:13:47 +0800

    add category images and objects for mapping

---
+++ src/assets/subCat1s.js
@@ -0,0 +1,86 @@
+export const subCat1s = [
+	{
+		img: require('./images/fd/babyfood.png'),
+		title: 'Baby Food'
+	},
+	{
+		img: require('./images/fd/babyformula.png'),
+		title: 'Baby Formula'
+	},
+	{
+		img: require('./images/fd/babysnacks.png'),
+		title: 'Baby Snacks'
+	},
+	{
+		img: require('./images/fd/baking.png'),
+		title: 'Baking'
+	},
+	{
+		img: require('./images/fd/bread.png'),
+		title: 'Bread'
+	},
+	{
+		img: require('./images/fd/breakfastsspreads.png'),
+		title: 'Breakfasts & Spreads'
+	},
+	{
+		img: require('./images/fd/buttermargarine.png'),
+		title: 'Butter & Margarine'
+	},
+	{
+		img: require('./images/fd/cakes.png'),
+		title: 'Cakes'
+	},
+	{
+		img: require('./images/fd/cannedfood.png'),
+		title: 'Canned Food'
+	},
+	{
+		img: require('./images/fd/catkitten.png'),
+		title: 'Cat & Kitten'
+	},
+	{
+		img: require('./images/fd/chilledpastasauce.png'),
+		title: 'Chilled Pasta Sauce'
+	},
+	{
+		img: require('./images/fd/chipshashbrownswedges.png'),
+		title: 'Chips, Hashbrowns & Wedges'
+	},
+	{
+		img: require('./images/fd/condiments.png'),
+		title: 'Condiments'
+	},
+	{
+		img: require('./images/fd/cookies&biscuits.png'),
+		title: 'Cookies & Biscuits'
+	},
+	{
+		img: require('./images/fd/cordial.png'),
+		title: 'Cordial'
+	},
+	{
+		img: require('./images/fd/creamcustarddesserts.png'),
+		title: 'Cream, Custard & Desserts'
+	},
+	{
+		img: require('./images/fd/crumpets&muffins.png'),
+		title: 'Crumpets & Muffins'
+	},
+	{
+		img: require('./images/fd/desserts&pastries.png'),
+		title: 'Desserts & Pastries'
+	},
+	{
+		img: require('./images/fd/dippatepaste.png'),
+		title: 'Dip, Pate & Paste'
+	},
+	{
+		img: require('./images/fd/dogpuppy.png'),
+		title: 'Dog & Puppy'
+	},
+	{
+		img: require('./images/fd/energydrinks.png'),
+		title: 'Energy Drinks'
+	}
+];
