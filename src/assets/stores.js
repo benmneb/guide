aa2c63b32784082c5c38279e8cdc00713201c46b
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-13 15:31:30 +0800

    add map to whereToBuy tab

--- src/assets/stores.js
+++ src/assets/stores.js
@@ -1,27 +1,52 @@
 export const stores = [
 	{
+		id: 1,
 		name: 'Woolworths, St Georges Terrace',
 		address: '101 St Georges Terrace, Perth, WA 6000',
+		coords: {
+			lat: -31.9546484,
+			lng: 115.8554883
+		},
 		isVegan: false
 	},
 	{
+		id: 2,
 		name: 'Coles, Raine Square',
 		address: 'Raine Square, 47 William St, Perth, WA 6000',
+		coords: {
+			lat: -31.9511603,
+			lng: 115.8551149
+		},
 		isVegan: false
 	},
 	{
+		id: 3,
 		name: 'The Loving Hut, Northbridge',
 		address: '132 James St, Northdbrige, WA 6000',
+		coords: {
+			lat: -31.9464252,
+			lng: 115.851813
+		},
 		isVegan: true
 	},
 	{
-		name: 'IGA, North Perth',
+		id: 4,
+		name: 'SUPA IGA, Perth',
 		address: '76 Barrack St, North Perth, WA 6000',
+		coords: {
+			lat: -31.9471339,
+			lng: 115.8616317
+		},
 		isVegan: false
 	},
 	{
+		id: 5,
 		name: 'La Vida Vegan, Subiaco',
 		address: '423 Rockeby Road, Subiaco WA, 6000',
+		coords: {
+			lat: -31.9476342,
+			lng: 115.8310174
+		},
 		isVegan: true
 	}
 ];
--- src/components/modals/ProductWhereToBuy.jsx
+++ src/components/modals/ProductWhereToBuy.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Grid, IconButton, Tooltip } from '@material-ui/core';
+import React, { useState, useEffect, useRef } from 'react';
+import { Grid, IconButton, Tooltip, Typography, Box } from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -15,92 +15,219 @@ import FileCopyRoundedIcon from '@material-ui/icons/FileCopyRounded';
 import { stores } from '../../assets/stores';
 import { makeStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
-	paper: {
-		padding: theme.spacing(2),
-		color: theme.palette.text.secondary
-	},
-	heading: {
-		color: theme.palette.text.primary,
-		fontWeight: theme.typography.fontWeightBold
-	},
 	listRoot: {
 		width: '100%',
 		backgroundColor: theme.palette.background.paper
 	},
 	nested: {
 		paddingLeft: theme.spacing(4)
+	},
+	map: {
+		height: '100%',
+		borderRadius: theme.shape.borderRadius
+	},
+	alert: {
+		width: '100%'
 	}
 }));
 
-export default function ProductAbout() {
-	const styles = useStyles();
-	const [open, setOpen] = useState('');
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
-	const handleClick = (store) => {
-		if (open === store) {
-			setOpen('');
+export default function ProductWhereToBuy() {
+	const styles = useStyles();
+	const [selectedStore, setSelectedStore] = useState(null);
+	const [mapPosition, setMapPosition] = useState(null);
+	const [mapZoom, setMapZoom] = useState(13);
+	const [errMessage, setErrMessage] = useState(null);
+	const [showSnack, setShowSnack] = useState(false);
+	const [showErrorSnack, setShowErrorSnack] = useState(false);
+
+	const handleCloseSnack = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setShowSnack(false);
+	};
+
+	const handleCloseErrorSnack = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setShowErrorSnack(false);
+	};
+
+	useEffect(() => {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) =>
+				setMapPosition({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				}),
+			(err) => setErrMessage(err.message)
+		);
+	}, []);
+
+	function PointsLayer(props) {
+		return props.data.map((store) => (
+			<PointMarker
+				key={store.id}
+				content={store.name}
+				position={store.coords}
+				openPopup={props.selectedMarker === store.id}
+			/>
+		));
+	}
+
+	function PointMarker(props) {
+		const markerRef = useRef(null);
+		const { openPopup } = props;
+
+		useEffect(() => {
+			if (openPopup) markerRef.current.leafletElement.openPopup();
+		}, [openPopup]);
+
+		return (
+			<Marker ref={markerRef} position={props.position}>
+				<Popup>{props.content}</Popup>
+			</Marker>
+		);
+	}
+
+	let mapDiv;
+	if (errMessage) {
+		mapDiv = <Typography>Error: {errMessage}</Typography>;
+	} else if (mapPosition) {
+		mapDiv = (
+			<Map
+				center={mapPosition}
+				zoom={mapZoom}
+				animate
+				duration="0.375"
+				className={styles.map}
+			>
+				<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				<PointsLayer selectedMarker={selectedStore} data={stores} />
+			</Map>
+		);
+	} else {
+		mapDiv = <Typography>Please accept the location request</Typography>;
+	}
+
+	const handleListItemClick = (store) => {
+		if (selectedStore === store.id) {
+			setSelectedStore(null);
 		} else {
-			setOpen(store);
+			setSelectedStore(store.id);
+			setMapPosition(store.coords);
+			setMapZoom(15);
 		}
 	};
 
+	function copyAddress(address) {
+		if (navigator.clipboard) {
+			navigator.clipboard
+				.writeText(address)
+				.then(setShowSnack(true))
+				.catch((err) => console.error(err));
+		} else {
+			setShowErrorSnack(true);
+		}
+	}
+
 	return (
-		<Grid container spacing={3}>
-			<Grid item xs={12} sm={6}>
-				map goes here
+		<>
+			<Grid container spacing={3}>
+				<Grid item xs={12} sm={6}>
+					<Box marginTop={2} height={475}>
+						{mapDiv}
+					</Box>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<List component="div" aria-label="Stores near you" className={styles.listRoot}>
+						{stores.map((store) => (
+							<div key={store.id}>
+								<ListItem
+									button
+									onClick={() => handleListItemClick(store)}
+									selected={selectedStore === store.id}
+								>
+									<ListItemIcon>
+										{store.isVegan ? (
+											<EcoRoundedIcon style={{ color: green[500] }} />
+										) : (
+											<PlaceIcon />
+										)}
+									</ListItemIcon>
+									<ListItemText primary={store.name} secondary={store.address} />
+									<ListItemSecondaryAction>
+										<Tooltip
+											title="Have you seen this product in this store?"
+											placement="bottom-end"
+										>
+											<IconButton aria-label="confirm">
+												<ThumbUpRoundedIcon fontSize="small" />
+											</IconButton>
+										</Tooltip>
+									</ListItemSecondaryAction>
+								</ListItem>
+								<Collapse in={selectedStore === store.id} timeout="auto" unmountOnExit>
+									<List component="div" dense style={{ paddingTop: 0 }}>
+										<ListItem button className={styles.nested}>
+											<ListItemIcon>
+												<LaunchRoundedIcon fontSize="small" />
+											</ListItemIcon>
+											<ListItemText primary="Get directions in Google Maps" />
+										</ListItem>
+										<ListItem
+											button
+											onClick={() => copyAddress(store.address)}
+											className={styles.nested}
+										>
+											<ListItemIcon>
+												<FileCopyRoundedIcon fontSize="small" />
+											</ListItemIcon>
+											<ListItemText primary="Copy address to clipboard" />
+										</ListItem>
+										<ListItem button className={styles.nested}>
+											<ListItemIcon>
+												<StoreIcon />
+											</ListItemIcon>
+											<ListItemText primary="See all products in this store" />
+										</ListItem>
+									</List>
+								</Collapse>
+							</div>
+						))}
+					</List>
+				</Grid>
 			</Grid>
-			<Grid item xs={12} sm={6}>
-				<List component="div" aria-label="Stores near you" className={styles.listRoot}>
-					{stores.map((store) => (
-						<div key={store.name}>
-							<ListItem button onClick={() => handleClick(store.name)}>
-								<ListItemIcon>
-									{store.isVegan ? (
-										<EcoRoundedIcon style={{ color: green[500] }} />
-									) : (
-										<PlaceIcon />
-									)}
-								</ListItemIcon>
-								<ListItemText primary={store.name} secondary={store.address} />
-								<ListItemSecondaryAction>
-									<Tooltip
-										title="Have you seen this product in this store?"
-										placement="bottom-end"
-									>
-										<IconButton aria-label="confirm">
-											<ThumbUpRoundedIcon fontSize="small" />
-										</IconButton>
-									</Tooltip>
-								</ListItemSecondaryAction>
-							</ListItem>
-							<Collapse in={open === store.name} timeout="auto" unmountOnExit>
-								<List component="div" dense>
-									<ListItem button className={styles.nested}>
-										<ListItemIcon>
-											<LaunchRoundedIcon fontSize="small" />
-										</ListItemIcon>
-										<ListItemText primary="Get directions in Google Maps" />
-									</ListItem>
-									<ListItem button className={styles.nested}>
-										<ListItemIcon>
-											<FileCopyRoundedIcon fontSize="small" />
-										</ListItemIcon>
-										<ListItemText primary="Copy address to clipboard" />
-									</ListItem>
-									<ListItem button className={styles.nested}>
-										<ListItemIcon>
-											<StoreIcon />
-										</ListItemIcon>
-										<ListItemText primary="See all products in this store" />
-									</ListItem>
-								</List>
-							</Collapse>
-						</div>
-					))}
-				</List>
-			</Grid>
-		</Grid>
+			<Snackbar
+				open={showSnack}
+				autoHideDuration={3000}
+				onClose={handleCloseSnack}
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+			>
+				<Alert onClose={handleCloseSnack} severity="info" className={styles.alert}>
+					Address copied to clipboard
+				</Alert>
+			</Snackbar>
+			<Snackbar
+				open={showErrorSnack}
+				autoHideDuration={3000}
+				onClose={handleCloseErrorSnack}
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+			>
+				<Alert onClose={handleCloseErrorSnack} severity="error" className={styles.alert}>
+					Could not copy to clipboard
+				</Alert>
+			</Snackbar>
+		</>
 	);
 }
