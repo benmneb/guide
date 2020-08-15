636538f03fb8cedff9fd70f53223e27898a801a4
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-15 17:18:39 +0800

    refactor

--- src/components/resultsList/Result.jsx
+++ src/components/resultsList/Result.jsx
@@ -1,106 +1,65 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography } from '@material-ui/core';
 import StarRating from './StarRating';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Result = (props) => (
-	<Card className={'ProductTile'} onClick={props.clicked}>
-		<CardMedia className={'MuiCardMedia-root'} component="img" image={props.image} />
-		<CardContent className={'MuiCardContent-root'}>
-			<Typography className={'MuiTypography--overline'} variant={'overline'} gutterBottom>
-				{props.brand}
-			</Typography>
-			<Typography className={'MuiTypography--heading'} variant={'h6'} noWrap>
-				{props.name}
-			</Typography>
-			{/* <Typography className={"MuiTypography--subheading"} variant={"caption"}>
-        caption etc
-      </Typography> */}
-		</CardContent>
-		<CardActions className={'MuiCardActions-root'}>
-			<StarRating />
-			{/* <Button color={"primary"} fullWidth>
-        View Full Info <Icon>chevron_right_rounded</Icon>
-      </Button> */}
-		</CardActions>
-	</Card>
-);
-
-Result.getTheme = (theme) => ({
-	MuiCard: {
-		root: {
-			'&.ProductTile': {
-				maxWidth: 300,
-				margin: 'auto',
-				transition: `${theme.transitions.duration.complex}ms ${theme.transitions.easing.easeInOut}`,
-				boxShadow: 'none',
-				borderRadius: theme.shape.borderRadius,
-				'& button': {
-					marginLeft: 0
-				},
-				'&:hover': {
-					transform: 'scale(1.04)',
-					boxShadow: theme.shadows[11],
-					cursor: 'pointer'
-				},
-				'& .MuiCardMedia-root': {
-					paddingTop: theme.spacing(),
-					maxHeight: 300,
-					position: 'relative',
-					'& .MuiTypography--category': {
-						color: 'rgba(255, 255, 255, 0.87)',
-						position: 'absolute',
-						top: theme.spacing(2.5),
-						left: theme.spacing(2.5),
-						letterSpacing: 0.5,
-						fontWeight: 300
-					}
-				},
-				'& .MuiCardContent-root': {
-					textAlign: 'center',
-					padding: theme.spacing(3),
-					'& .MuiTypography--overline': {
-						// brand name
-						color: theme.palette.grey[500],
-						fontWeight: theme.typography.fontWeightBold
-					},
-					'& .MuiTypography--heading': {
-						/// product title
-						fontWeight: theme.typography.fontWeightLight,
-						lineHeight: 1.3
-					},
-					'& .MuiTypography--subheading': {
-						// ???
-						lineHeight: 1.8,
-						color: 'green',
-						fontWeight: theme.typography.fontWeightBold
-					}
-				},
-				'& .MuiCardActions-root': {
-					padding: `0 ${theme.spacing(3)}px ${theme.spacing(3)}px`,
-					margin: 'auto',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					flexDirection: 'column'
-				}
-			}
+const useStyles = makeStyles((theme) => ({
+	productTile: {
+		maxWidth: 300,
+		margin: 'auto',
+		transition: `${theme.transitions.duration.complex}ms ${theme.transitions.easing.easeInOut}`,
+		boxShadow: 'none',
+		borderRadius: theme.shape.borderRadius,
+		'&:hover': {
+			transform: 'scale(1.04)',
+			boxShadow: theme.shadows[11],
+			cursor: 'pointer'
 		}
 	},
-	MuiButton: {
-		root: {
-			'& svg, .material-icons': {
-				marginLeft: theme.spacing()
-			}
-		},
-		label: {
-			textTransform: 'initial'
-		}
+	cardMedia: {
+		paddingTop: theme.spacing(),
+		maxHeight: 300,
+		position: 'relative'
+	},
+	cardContent: {
+		textAlign: 'center',
+		padding: theme.spacing(3)
+	},
+	brandName: {
+		color: theme.palette.grey[500],
+		fontWeight: theme.typography.fontWeightBold
+	},
+	productName: {
+		fontWeight: theme.typography.fontWeightRegular,
+		lineHeight: 1.3
+	},
+	cardActions: {
+		padding: `0 ${theme.spacing(3)}px ${theme.spacing(3)}px`,
+		margin: 'auto',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'column'
 	}
-});
+}));
 
-Result.metadata = {
-	name: 'News Card',
-	description: 'Best for Blog'
-};
+export default function Result(props) {
+	const styles = useStyles();
 
-export default Result;
+	return (
+		<Card className={styles.productTile} onClick={props.clicked}>
+			<CardMedia className={styles.cardMedia} component="img" image={props.image} />
+			<CardContent className={styles.cardContent}>
+				<Typography className={styles.brandName} variant="overline" gutterBottom>
+					{props.brand}
+				</Typography>
+				<Typography className={styles.productName} variant="h6" component="p">
+					{props.name}
+				</Typography>
+			</CardContent>
+			<CardActions className={styles.cardActions}>
+				<StarRating />
+			</CardActions>
+		</Card>
+	);
+}
--- src/components/resultsList/ResultsList.jsx
+++ src/components/resultsList/ResultsList.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
 import { connect } from 'react-redux';
-import { makeStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Result from './Result';
 import Hero, { Heading, SubHeading, Footer } from '../Hero/Hero';
 import FiltersBar from '../AppBar/FiltersBar';
 import AddProductsFab from './AddProductsFab';
 import * as actionCreators from '../../store/actions';
-
-const muiBaseTheme = createMuiTheme();
 
 const drawerWidth = 430;
 
@@ -57,74 +55,65 @@ const ResultsList = (props) => {
 					[styles.containerShift]: props.showFiltersPanel
 				})}
 			>
-				<MuiThemeProvider
-					theme={createMuiTheme({
-						typography: {
-							useNextVariants: true
-						},
-						overrides: Result.getTheme(muiBaseTheme)
-					})}
-				>
-					<Result
-						image="https://mouthsofmums.com.au/wp-content/uploads/2016/05/046731-300x300.jpg"
-						brand="Kraft"
-						name="Peanut Butter - Crunchy"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://southburnett.com.au/news2/wp-content/uploads/2017/01/kraft1.jpg"
-						brand="Kraft"
-						name="Peanut Butter - Smooth"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://shop.countdown.co.nz/Content/ProductImages/zoom/9421901881054.jpg/Pics-Peanut-Butter-Crunchy.jpg"
-						brand="Pic's"
-						name="Really Good Peanut Butter - Crunchy Freshly Roasted and Lovingly Squashed"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://i5.walmartimages.com/asr/c7806ff6-536e-4f37-8653-95d08d630fdb_1.5d608b379a7ef87af70460cb56c02bcf.jpeg"
-						brand="Tofurky"
-						name="Deli Slices - Original Recipe Savoury Plant-Based"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://mouthsofmums.com.au/wp-content/uploads/2016/05/046731-300x300.jpg"
-						brand="Kraft"
-						name="Peanut Butter - Crunchy"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://southburnett.com.au/news2/wp-content/uploads/2017/01/kraft1.jpg"
-						brand="Kraft"
-						name="Peanut Butter - Smooth"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://shop.countdown.co.nz/Content/ProductImages/zoom/9421901881054.jpg/Pics-Peanut-Butter-Crunchy.jpg"
-						brand="Pic's"
-						name="Peanut Butter - Crunchy"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result
-						image="https://i5.walmartimages.com/asr/c7806ff6-536e-4f37-8653-95d08d630fdb_1.5d608b379a7ef87af70460cb56c02bcf.jpeg"
-						brand="Tofurky"
-						name="Deli Slices - Original Recipe Savoury Plant-Based"
-						clicked={() => props.onToggleProductModal()}
-					/>
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-					<Result />
-				</MuiThemeProvider>
+				<Result
+					image="https://mouthsofmums.com.au/wp-content/uploads/2016/05/046731-300x300.jpg"
+					brand="Kraft"
+					name="Peanut Butter - Crunchy"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://southburnett.com.au/news2/wp-content/uploads/2017/01/kraft1.jpg"
+					brand="Kraft"
+					name="Peanut Butter - Smooth"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://shop.countdown.co.nz/Content/ProductImages/zoom/9421901881054.jpg/Pics-Peanut-Butter-Crunchy.jpg"
+					brand="Pic's"
+					name="Really Good Peanut Butter - Crunchy Freshly Roasted and Lovingly Squashed"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://i5.walmartimages.com/asr/c7806ff6-536e-4f37-8653-95d08d630fdb_1.5d608b379a7ef87af70460cb56c02bcf.jpeg"
+					brand="Tofurky"
+					name="Deli Slices - Original Recipe Savoury Plant-Based"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://mouthsofmums.com.au/wp-content/uploads/2016/05/046731-300x300.jpg"
+					brand="Kraft"
+					name="Peanut Butter - Crunchy"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://southburnett.com.au/news2/wp-content/uploads/2017/01/kraft1.jpg"
+					brand="Kraft"
+					name="Peanut Butter - Smooth"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://shop.countdown.co.nz/Content/ProductImages/zoom/9421901881054.jpg/Pics-Peanut-Butter-Crunchy.jpg"
+					brand="Pic's"
+					name="Peanut Butter - Crunchy"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result
+					image="https://i5.walmartimages.com/asr/c7806ff6-536e-4f37-8653-95d08d630fdb_1.5d608b379a7ef87af70460cb56c02bcf.jpeg"
+					brand="Tofurky"
+					name="Deli Slices - Original Recipe Savoury Plant-Based"
+					clicked={() => props.onToggleProductModal()}
+				/>
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
+				<Result />
 			</div>
 			<AddProductsFab />
 		</>
