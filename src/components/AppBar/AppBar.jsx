53152a3404f79a47622dfb0fd44216ff59de51cd
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-19 17:30:20 +0800

    change searchbar bg colour to suit hero images

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -34,9 +34,9 @@ const useStyles = makeStyles((theme) => ({
 	search: {
 		position: 'relative',
 		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.black, 0.15),
+		backgroundColor: fade(theme.palette.common.white, 0.55), // was black 0.15
 		'&:hover': {
-			backgroundColor: fade(theme.palette.common.black, 0.25)
+			backgroundColor: fade(theme.palette.common.white, 0.75) // was black 0.25
 		},
 		marginLeft: 0,
 		width: '100%',
