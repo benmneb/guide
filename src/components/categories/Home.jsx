5d524dae03509629e1812a37f049f8d158bf4f7f
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-08-22 18:42:55 +0800

    actually remove home footer

--- src/components/categories/Home.jsx
+++ src/components/categories/Home.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-	GridList,
-	GridListTile,
-	GridListTileBar,
-	Typography,
-	Toolbar,
-	Button,
-	Box
-} from '@material-ui/core';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { GridList, GridListTile, GridListTileBar, Box } from '@material-ui/core';
 import { subCat1s } from '../../assets/subCat1s';
-import Hero, { Heading, SubHeading, Footer } from '../Hero/Hero';
+import Hero, { Heading, SubHeading } from '../Hero/Hero';
 
 const useStyles = makeStyles((theme) => ({
+	container: {
+		padding: theme.spacing(3),
+		display: 'grid',
+		gridGap: theme.spacing(2),
+		gridTemplateColumns: `repeat(auto-fit, minmax(${theme.spacing(31)}, 1fr))`
+	},
+	root: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		backgroundColor: theme.palette.common.white,
+		marginTop: theme.spacing(2)
+	},
 	gridList: {
-		flexWrap: 'nowrap',
-		// Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-		transform: 'translateZ(0)'
+		width: '100%'
 	},
 	gridListTile: {
 		'&:hover img': {
@@ -45,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
-export default function SingleLineGridList() {
+const SubCat1s = () => {
 	const styles = useStyles();
 
 	return (
@@ -53,55 +54,32 @@ export default function SingleLineGridList() {
 			<Hero textAlign="center">
 				<Heading>Find Vegan Products</Heading>
 				<SubHeading>
-					The Vomad Guide is a free community-sourced collection of 100% plant-based
-					products. There are 12,815 vegan products in 212 categories in 576 stores and 54
-					online stores within Australia.
+					The Vomad Guide is a free crowd-sourced collection of 100% plant-based products.
+					There are 12,815 vegan products in 212 categories in 576 stores and 54 online
+					stores within Australia.
 				</SubHeading>
-				<Footer forHome />
 			</Hero>
-			<Box marginY={-4}>
-				{[
-					'Popular Categories',
-					'Highly Rated',
-					'Gluten Free',
-					'Recently Added',
-					'Mock Meat Categories',
-					'Highest Rated Snacks & Confectionary',
-					'Pet Food'
-				].map((category) => (
-					<Box marginY={4} key={category}>
-						<Toolbar>
-							<Box flexGrow="1">
-								<Typography variant="h5" align="left">
-									{category}
-								</Typography>
-							</Box>
-							<Box flexGrow="0">
-								<Link to="/food-drink/nut-butters-spreads">
-									<Button variant="text" color="default" endIcon={<ChevronRightIcon />}>
-										See all {category}
-									</Button>
-								</Link>
-							</Box>
-						</Toolbar>
-						<GridList className={styles.gridList} cols={5} cellHeight={300} spacing={0}>
-							{subCat1s.map((image) => (
-								<GridListTile key={image.img} cols={1} className={styles.gridListTile}>
-									<img src={image.img} alt={image.title} className={styles.image} />
-									<GridListTileBar
-										titlePosition="top"
-										title={image.title}
-										className={styles.titleBar}
-										classes={{
-											title: styles.title
-										}}
-									/>
-								</GridListTile>
-							))}
-						</GridList>
-					</Box>
-				))}
+			<Box className={styles.root}>
+				<GridList cellHeight={300} className={styles.gridList} cols={5} spacing={0}>
+					{subCat1s.map((image) => (
+						<GridListTile key={image.img} cols={1} className={styles.gridListTile}>
+							<Link to="/food-drink/nut-butters-spreads">
+								<img src={image.img} alt={image.title} className={styles.image} />
+								<GridListTileBar
+									titlePosition="top"
+									title={image.title}
+									className={styles.titleBar}
+									classes={{
+										title: styles.title
+									}}
+								/>
+							</Link>
+						</GridListTile>
+					))}
+				</GridList>
 			</Box>
 		</>
 	);
-}
+};
+
+export default SubCat1s;
