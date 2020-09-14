179947d259013c42d71f5ea6cbcfb079ce2477fd
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-09-14 17:56:58 +0800

    change local links to imageKit links

--- src/assets/subCat1s.js
+++ src/assets/subCat1s.js
@@ -1,86 +1,107 @@
 export const subCat1s = [
 	{
-		img: require('./images/fd/babyfood.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/babyfood__bW7f2swmm.png',
 		title: 'Baby Food'
 	},
 	{
-		img: require('./images/fd/babyformula.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/babyformula_C96_XNHLoP.png',
 		title: 'Baby Formula'
 	},
 	{
-		img: require('./images/fd/babysnacks.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/babysnacks_vVTskIv3t.png',
 		title: 'Baby Snacks'
 	},
 	{
-		img: require('./images/fd/baking.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/baking_MRCbflIByM.png',
 		title: 'Baking'
 	},
 	{
-		img: require('./images/fd/bread.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/bread_srHZjRCN3u.png',
 		title: 'Bread'
 	},
 	{
-		img: require('./images/fd/breakfastsspreads.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/breakfastsspreads_jQ5Rkefe8C.png',
 		title: 'Breakfasts & Spreads'
 	},
 	{
-		img: require('./images/fd/buttermargarine.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/buttermargarine_Wknq-l-7vt.png',
 		title: 'Butter & Margarine'
 	},
 	{
-		img: require('./images/fd/cakes.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/cakes_VJBEL5qsiJ.png',
 		title: 'Cakes'
 	},
 	{
-		img: require('./images/fd/cannedfood.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/cannedfood_F-FDVYXru.png',
 		title: 'Canned Food'
 	},
 	{
-		img: require('./images/fd/catkitten.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/catkitten_zRbTKeQQyA.png',
 		title: 'Cat & Kitten'
 	},
 	{
-		img: require('./images/fd/chilledpastasauce.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/chilledpastasauce_xbCjWZ-Ot.png',
 		title: 'Chilled Pasta Sauce'
 	},
 	{
-		img: require('./images/fd/chipshashbrownswedges.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/chipshashbrownswedges_IzV5xjFDA-.png',
 		title: 'Chips, Hashbrowns & Wedges'
 	},
 	{
-		img: require('./images/fd/condiments.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/condiments_3Prxed-Rfw.png',
 		title: 'Condiments'
 	},
 	{
-		img: require('./images/fd/cookies&biscuits.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/cookies_biscuits_SGTT5cgo1r.png',
 		title: 'Cookies & Biscuits'
 	},
 	{
-		img: require('./images/fd/cordial.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/cordial_6hghpLwXd.png',
 		title: 'Cordial'
 	},
 	{
-		img: require('./images/fd/creamcustarddesserts.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/creamcustarddesserts_slCvHBjTq4.png',
 		title: 'Cream, Custard & Desserts'
 	},
 	{
-		img: require('./images/fd/crumpets&muffins.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/crumpets_muffins_R1JqnQwxzk.png',
 		title: 'Crumpets & Muffins'
 	},
 	{
-		img: require('./images/fd/desserts&pastries.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/desserts_pastries_9QlVnGnsi-.png',
 		title: 'Desserts & Pastries'
 	},
 	{
-		img: require('./images/fd/dippatepaste.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/dippatepaste_MObUyHQtPB.png',
 		title: 'Dip, Pate & Paste'
 	},
 	{
-		img: require('./images/fd/dogpuppy.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/dogpuppy_eyIDe4aLTl.png',
 		title: 'Dog & Puppy'
 	},
 	{
-		img: require('./images/fd/energydrinks.png'),
+		img:
+			'https://ik.imagekit.io/vomadguide/subcat1s/tr:n-subcat1_upXS/energydrinks_oLuv_Y6WbI.png',
 		title: 'Energy Drinks'
 	}
 ];
