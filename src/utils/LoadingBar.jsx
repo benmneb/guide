b540f07ce82f597b27e7b7668adb179625b44aec
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-09-24 18:39:16 +0800

    change position to fixed

--- src/utils/LoadingBar.jsx
+++ src/utils/LoadingBar.jsx
@@ -5,7 +5,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => ({
 	loadingBar: {
-		position: 'absolute',
+		position: 'fixed',
 		width: '100%',
 		zIndex: theme.zIndex.tooltip
 	}
@@ -15,9 +15,11 @@ export default function LoadingBar() {
 	const styles = useStyles();
 	const isLoading = useSelector((state) => state.isLoading);
 
-	return isLoading ? (
-		<div className={styles.loadingBar}>
-			<LinearProgress />
-		</div>
-	) : null;
+	return (
+		isLoading && (
+			<div className={styles.loadingBar}>
+				<LinearProgress />
+			</div>
+		)
+	);
 }
