11230caf44ba209bf3a4af4fa7a6c77cd6b259d8
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-10-26 08:39:31 +0800

    add react-snap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
@@ -16,9 +16,13 @@ const app = (
 	</Provider>
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+if (rootElement.hasChildNodes()) {
+	hydrate(app, rootElement);
+} else {
+	render(app, rootElement);
+}
+
 serviceWorker.register();
+// Learn more about service workers: https://bit.ly/CRA-PWA
