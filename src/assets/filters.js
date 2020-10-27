0732aecae9e80f638f5c74a89363228e5d367aba
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-10-27 19:24:33 +0800

    update tooltips

--- src/assets/filters.js
+++ src/assets/filters.js
@@ -3,25 +3,25 @@ export const tags = [
 		id: 1,
 		name: '🌱 Vegan Brands',
 		value: 'Vegan Brands',
-		tooltip: 'Show only products from vegan brands'
+		tooltip: 'Only show products from vegan brands'
 	},
 	{
 		id: 5,
-		name: '📍 Local Brands',
+		name: '🇦🇺 Local Brands',
 		value: 'Local Brands',
-		tooltip: 'Show only brands owned in your country'
+		tooltip: 'Only show brands owned in Australia'
 	},
 	{
 		id: 7,
 		name: '👩‍🦰 For Women',
 		value: 'For Women',
-		tooltip: 'Only applies to household items, not food / drink'
+		tooltip: 'Only applies to household items, not food or drink'
 	},
 	{
 		id: 6,
 		name: '👨 For Men',
 		value: 'For Men',
-		tooltip: 'Only applies to household items, not food / drink'
+		tooltip: 'Only applies to household items, not food or drink'
 	}
 ];
 
@@ -36,13 +36,14 @@ export const ingredients = [
 		id: 9,
 		name: '🦷 No Added Sugar',
 		value: 'No Added Sugar',
-		tooltip: "Ingredients list does not contain 'sugar'"
+		tooltip:
+			"Ingredients list does not contain 'sugar', 'sucrose', 'glucose', 'fructose' or 28 other synonyms and ingredients known to contain sugar"
 	},
 	{
 		id: 21,
 		name: '🧂 No Salt',
 		value: 'No Salt',
-		tooltip: "Ingredients list does not contain 'salt' or 'sodium'"
+		tooltip: "Ingredients list does not contain 'salt', 'sodium' or 'fleur de sel'"
 	},
 	{
 		id: 4,
@@ -57,72 +58,81 @@ export const allergens = [
 		id: 2,
 		name: '🍞 Gluten Free',
 		value: 'Gluten Free',
-		tooltip: "Ingredients list does not contain 'gluten' etc"
+		tooltip:
+			"Ingredients list does not contain 'gluten' or 22 other ingredients known to contain gluten"
 	},
 	{
 		id: 10,
 		name: '🌾 Wheat Free',
 		value: 'Wheat Free',
-		tooltip: "Ingredients list does not contain 'wheat' etc"
+		tooltip:
+			"Ingredients list does not contain 'wheat' or 39 other synonyms and ingredients known to contain wheat"
 	},
 	{
 		id: 11,
 		name: '🥜 Peanut Free',
 		value: 'Peanut Free',
-		tooltip: "Ingredients list does not contain 'peanuts' etc"
+		tooltip:
+			"Ingredients list does not contain 'peanut' or 11 other synonyms and ingredients known to contain peanuts"
 	},
 	{
 		id: 12,
 		name: '🌰 Tree-Nut Free',
 		value: 'Tree-Nut Free',
-		tooltip: "Ingredients list does not contain 'tree nuts' etc"
+		tooltip:
+			"Ingredients list does not contain 'almond', 'brazil nut', 'cashew', 'hazelnut', 'macadamia', 'pecan', 'pistachio', 'walnut' or 18 other tree-nuts and ingredients known to contain tree-nuts"
 	},
 	{
 		id: 13,
-		name: '🤢 Lupin Free',
-		value: 'Lupin Free',
-		tooltip: "Ingredients list does not contain 'lupin' etc"
+		name: '🤮 Soy Free',
+		value: 'Soy Free',
+		tooltip:
+			"Ingredients list does not contain 'soy', 'soja' or 13 other synonyms and ingredients known to contain soy"
 	},
 	{
 		id: 14,
-		name: '🤮 Soy Free',
-		value: 'Soy Free',
-		tooltip: "Ingredients list does not contain 'soy' etc"
+		name: '🤢 Lupin Free',
+		value: 'Lupin Free',
+		tooltip: "Ingredients list does not contain 'lupin' or 2 other synonyms"
 	},
 	{
 		id: 15,
 		name: '🤒 Sesame Free',
 		value: 'Sesame Free',
-		tooltip: "Ingredients list does not contain 'sesame' etc"
+		tooltip:
+			"Ingredients list does not contain 'sesame' or 11 other synonyms and ingredients known to contain sesame"
 	},
 	{
 		id: 16,
 		name: '🥴 Mustard Free',
 		value: 'Mustard Free',
-		tooltip: "Ingredients list does not contain 'mustard' etc"
+		tooltip:
+			"Ingredients list does not contain 'mustard' or 2 other ingredients known to contain mustard"
 	},
 	{
 		id: 17,
 		name: '😵 Alcohol Free',
 		value: 'Alcohol Free',
-		tooltip: "Ingredients list does not contain 'alcohol' etc"
+		tooltip:
+			"Ingredients list does not contain 'alcohol', 'decanol', 'ethanol', 'methanol', or 16 other synonyms"
 	},
 	{
 		id: 18,
 		name: '😟 Phthalate Free',
 		value: 'Phthalate Free',
-		tooltip: "Ingredients list does not contain 'phthalate' etc"
+		tooltip: "Ingredients list does not contain 'phthalate' or 8 other synonyms"
 	},
 	{
 		id: 19,
 		name: '😳 Paraben Free',
 		value: 'Paraben Free',
-		tooltip: "Ingredients list does not contain 'paraben' etc"
+		tooltip: "Ingredients list does not contain 'paraben' or 10 other synonyms"
 	},
 	{
 		id: 20,
 		name: '🤕 SLS/SLES Free',
 		value: 'SLS/SLES Free',
-		tooltip: "Ingredients list does not contain 'sodium laurel sulphate' etc"
+		tooltip:
+			"Ingredients list does not contain 'sodium lauryl sulphate', 'sodium lauryl ether sulfate' or 15 other synonyms and ingredients known to contain SLS"
 	}
 ];
