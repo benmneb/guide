78ac6de798f6dba1ab93b702df6da6be2d9b9aba
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-11-11 12:05:20 +0800

    add createGenerateClassName

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConfirmProvider } from 'material-ui-confirm';
-import { ThemeProvider } from '@material-ui/core/styles';
+import {
+	ThemeProvider,
+	StylesProvider,
+	createGenerateClassName
+} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { defaultOptions } from './assets/confirmProviderOptions';
 import { theme } from './assets/theme';
@@ -18,43 +22,49 @@ const SearchResultsList = lazy(() =>
 	import('./components/ResultsList/SearchResultsList')
 );
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'vmd.gd'
+});
+
 export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<ConfirmProvider defaultOptions={defaultOptions}>
-				<CssBaseline />
-				<LoadingBar />
-				<AppBar>
-					<Switch>
-						<Route exact path="/">
-							<Categories />
-						</Route>
-						<Route exact path="/auth/success">
-							<AuthSuccess />
-						</Route>
-						<Route path="/search/:term">
-							<Suspense fallback={<ResultsListSkeleton />}>
-								<SearchResultsList />
-							</Suspense>
-						</Route>
-						<Route path="/:productType/:category">
-							<Suspense fallback={<ResultsListSkeleton />}>
-								<ResultsList />
-							</Suspense>
-						</Route>
-						<Route path="/:productType">
-							<Categories />
-						</Route>
-						<Route>
-							<PageNotFound />
-						</Route>
-					</Switch>
-				</AppBar>
-				<Suspense fallback={null}>
-					<GetParameterPopups />
-				</Suspense>
-				<Snackbars />
-			</ConfirmProvider>
+			<StylesProvider generateClassName={generateClassName}>
+				<ConfirmProvider defaultOptions={defaultOptions}>
+					<CssBaseline />
+					<LoadingBar />
+					<AppBar>
+						<Switch>
+							<Route exact path="/">
+								<Categories />
+							</Route>
+							<Route exact path="/auth/success">
+								<AuthSuccess />
+							</Route>
+							<Route path="/search/:term">
+								<Suspense fallback={<ResultsListSkeleton />}>
+									<SearchResultsList />
+								</Suspense>
+							</Route>
+							<Route path="/:productType/:category">
+								<Suspense fallback={<ResultsListSkeleton />}>
+									<ResultsList />
+								</Suspense>
+							</Route>
+							<Route path="/:productType">
+								<Categories />
+							</Route>
+							<Route>
+								<PageNotFound />
+							</Route>
+						</Switch>
+					</AppBar>
+					<Suspense fallback={null}>
+						<GetParameterPopups />
+					</Suspense>
+					<Snackbars />
+				</ConfirmProvider>
+			</StylesProvider>
 		</ThemeProvider>
 	);
 }
